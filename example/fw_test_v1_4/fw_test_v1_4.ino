334b75b320bc26e746f622c8279b9843c98d95f5
Makerfabs <32691976+Makerfabs@users.noreply.github.com>
2025-11-19 17:16:53 +0800

    firmware test for hardware v1.2(add RTC PCF8563)

---
+++ example/fw_test_v1_4/fw_test_v1_4.ino
@@ -0,0 +1,375 @@
+
+
+
+
+
+
+// Using library Wire at version 3.2.0 in folder: C:\Users\miand\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.2.0\libraries\Wire 
+// Using library Adafruit Unified Sensor at version 1.1.15 in folder: C:\Users\miand\Documents\Arduino\libraries\Adafruit_Unified_Sensor 
+// Using library Adafruit AHT10 at version 0.1.0 in folder: C:\Users\miand\Documents\Arduino\libraries\Adafruit_AHT10 
+// Using library Adafruit BusIO at version 1.17.1 in folder: C:\Users\miand\Documents\Arduino\libraries\Adafruit_BusIO 
+// Using library SPI at version 3.2.0 in folder: C:\Users\miand\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.2.0\libraries\SPI 
+// Using library Adafruit SGP30 Sensor at version 2.0.3 in folder: C:\Users\miand\Documents\Arduino\libraries\Adafruit_SGP30_Sensor 
+// Using library BH1750 at version 1.3.0 in folder: C:\Users\miand\Documents\Arduino\libraries\BH1750 
+// Using library SensorLib at version 0.2.6 in folder: C:\Users\miand\Documents\Arduino\libraries\SensorLib 
+
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_AHT10.h>
+#include <Adafruit_SGP30.h>
+#include <BH1750.h>
+#include "SensorPCF8563.hpp"
+
+#define DEBUG true
+
+#define IO_POWER_3V3 21
+#define IO_POWER_1V8 48
+
+#define IO_SDA 17
+#define IO_SCL 18
+
+#define IO_RTC_IRQ  13
+
+#define IO_BUTTON_PIN 8
+#define IO_LED_PIN 47
+
+
+
+#define IO_BAT_PIN 14
+
+#define IO_RXD2 1
+#define IO_TXD2 2
+
+#define IO_GSM_PWRKEY 42
+#define IO_GSM_RST 41
+
+#define DBG_OUT  Serial //Serial
+HardwareSerial mySerial2(2);
+
+Adafruit_AHT10 aht;
+Adafruit_SGP30 sgp;
+BH1750 lightMeter;
+
+SensorPCF8563 rtc;
+
+uint32_t lastMillis;
+
+
+float temperature = 0.0;
+float humidity = 0.0;
+float tvoc = 0.0;
+float eCO2 = 0.0;
+float H2 = 0.0;
+float Ethanol = 0.0;
+float lux = 0.0;
+float bat_vol = 0.0;
+int count = 0;
+
+int init_flag_aht10 = 0;
+int init_flag_SGP30 = 0;
+int init_flag_BH1750 = 0;
+int init_flag_PCF8563 = 0;
+//bool gLED_state = 1;
+
+unsigned long getTime1 = 0; // 记录当前毫秒时间
+const long intervalTime = 3000; // 3秒间隔
+void setup()
+{
+
+    DBG_OUT.begin(115200);
+    mySerial2.begin(115200, SERIAL_8N1, IO_RXD2, IO_TXD2);
+
+    pin_init();
+
+    sensor_init();
+
+    while (1)
+    {
+
+        if (digitalRead(IO_BUTTON_PIN) == 0)
+        {
+            delay(100);
+            if (digitalRead(IO_BUTTON_PIN) == 0)
+            {
+                DBG_OUT.println("Button press down.");
+                break;
+            }
+        }
+        else
+        {
+            sensor_read();
+            value_report();
+            delay(1000);
+        }
+    }
+    for(int i=0;i<10;i++)
+    {
+        sensor_read();
+        value_report();
+        delay(1000);
+    }
+
+    at_init();
+    http_request();
+    getTime1 = millis(); // 记录当前毫秒时间
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    
+    if(millis()-getTime1>intervalTime)
+    {
+      getTime1 = millis();
+      sensor_read();
+      value_report();
+      digitalWrite(IO_LED_PIN, !digitalRead(IO_LED_PIN));
+    }
+
+    while (DBG_OUT.available() > 0)
+    {
+        mySerial2.write(DBG_OUT.read());
+        yield();
+    }
+    while (mySerial2.available() > 0)
+    {
+        DBG_OUT.write(mySerial2.read());
+        yield();
+    }
+}
+
+void pin_init()
+{
+    pinMode(IO_POWER_3V3, OUTPUT);
+    pinMode(IO_POWER_1V8, OUTPUT);
+    pinMode(IO_BUTTON_PIN, INPUT);
+    pinMode(IO_LED_PIN, OUTPUT);
+    pinMode(IO_BAT_PIN, INPUT);
+
+    pinMode(IO_GSM_RST, OUTPUT);
+    pinMode(IO_GSM_PWRKEY, OUTPUT);
+
+    digitalWrite(IO_POWER_3V3, HIGH);
+    digitalWrite(IO_POWER_1V8, HIGH);
+    digitalWrite(IO_LED_PIN, HIGH);
+
+    digitalWrite(IO_GSM_RST, HIGH);
+    delay(1000);
+    digitalWrite(IO_GSM_RST, LOW);
+    delay(500);
+    digitalWrite(IO_GSM_PWRKEY, HIGH);
+    delay(1000);
+    digitalWrite(IO_GSM_PWRKEY, LOW);
+}
+
+void sensor_init()
+{
+    DBG_OUT.println("Sensor init begin.");
+
+//    Wire.begin(IO_SDA, IO_SCL);
+
+    //if (!rtc.begin(Wire, PCF8563_SLAVE_ADDRESS, SENSOR_SDA, SENSOR_SCL))
+    if (!rtc.begin(Wire, PCF8563_SLAVE_ADDRESS,IO_SDA,IO_SCL))
+        DBG_OUT.println("PCF8563 not found.");
+    else
+    {
+        uint16_t year = 2025;
+        uint8_t month = 11;
+        uint8_t day = 13;
+        uint8_t hour = 21;
+        uint8_t minute = 00;
+        uint8_t second = 30;
+
+        rtc.setDateTime(year, month, day, hour, minute, second);
+        init_flag_PCF8563 = 1;
+    }
+
+    if (!aht.begin())
+        DBG_OUT.println("SGP30 not found.");
+    else
+        init_flag_aht10 = 1;
+
+    if (!sgp.begin())
+        DBG_OUT.println("SGP30 not found.");
+    else
+        init_flag_SGP30 = 1;
+
+    if (!lightMeter.begin())
+        DBG_OUT.println("BH1750 not found.");
+    else
+        init_flag_BH1750 = 1;
+
+    DBG_OUT.println("Sensor init over.");
+}
+
+void sensor_read()
+{
+
+    if (init_flag_aht10)
+        read_aht10();
+    if (init_flag_SGP30)
+        read_SGP30();
+    if (init_flag_BH1750)
+        read_BH1750();
+    if (init_flag_PCF8563)
+        read_PCF8563();    
+    read_bat();
+    count++;
+}
+
+uint32_t getAbsoluteHumidity(float temperature, float humidity)
+{
+    // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
+    const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+    const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity);                                                                // [mg/m^3]
+    return absoluteHumidityScaled;
+}
+
+void read_aht10()
+{
+    sensors_event_t humi, temp;
+    aht.getEvent(&humi, &temp); // populate temp and humidity objects with fresh data
+    temperature = temp.temperature;
+    humidity = humi.relative_humidity;
+}
+
+void read_SGP30()
+{
+    sgp.setHumidity(getAbsoluteHumidity(temperature, humidity));
+
+    if (!sgp.IAQmeasure())
+    {
+        DBG_OUT.println("Measurement failed");
+        return;
+    }
+    tvoc = sgp.TVOC;
+    eCO2 = sgp.eCO2;
+
+    if (!sgp.IAQmeasureRaw())
+    {
+        DBG_OUT.println("Raw Measurement failed");
+        return;
+    }
+    H2 = sgp.rawH2;
+    Ethanol = sgp.rawEthanol;
+}
+
+void read_BH1750()
+{
+    lux = lightMeter.readLightLevel();
+}
+
+void read_PCF8563()
+{
+    RTC_DateTime datetime = rtc.getDateTime();
+    Serial.printf(" Year :"); Serial.print(datetime.year);
+    Serial.printf(" Month:"); Serial.print(datetime.month);
+    Serial.printf(" Day :"); Serial.print(datetime.day);
+    Serial.printf(" Hour:"); Serial.print(datetime.hour);
+    Serial.printf(" Minute:"); Serial.print(datetime.minute);
+    Serial.printf(" Sec :"); Serial.println(datetime.second);
+
+}
+
+
+void read_bat()
+{
+    bat_vol = 3.3 * analogRead(IO_BAT_PIN) / 4096 * 2 + 0.3;
+}
+
+void value_report()
+{
+    DBG_OUT.println("\n-------------------------------------");
+    DBG_OUT.printf("Num:\t%d\n", count);
+
+    DBG_OUT.print("Temperature: ");
+    DBG_OUT.print(temperature);
+    DBG_OUT.println(" degrees C");
+    DBG_OUT.print("Humidity: ");
+    DBG_OUT.print(humidity);
+    DBG_OUT.println("% rH");
+
+    DBG_OUT.print("TVOC ");
+    DBG_OUT.print(tvoc);
+    DBG_OUT.print(" ppb\t");
+    DBG_OUT.print("eCO2 ");
+    DBG_OUT.print(eCO2);
+    DBG_OUT.println(" ppm");
+
+    DBG_OUT.print("Raw H2 ");
+    DBG_OUT.print(H2);
+    DBG_OUT.print(" \t");
+    DBG_OUT.print("Raw Ethanol ");
+    DBG_OUT.print(Ethanol);
+    DBG_OUT.println("");
+
+    DBG_OUT.print("Light: ");
+    DBG_OUT.print(lux);
+    DBG_OUT.println(" lx");
+
+    DBG_OUT.print("Bat: ");
+    DBG_OUT.print(bat_vol);
+    DBG_OUT.println(" V");
+
+    DBG_OUT.println("\n-------------------------------------");
+
+    // 14:37:57.103 -> Num:	5
+    // 14:37:57.103 -> Temperature: 28.98 degrees C
+    // 14:37:57.103 -> Humidity: 44.40% rH
+    // 14:37:57.103 -> TVOC 0.00 ppb	eCO2 400.00 ppm
+    // 14:37:57.103 -> Raw H2 13978.00 	Raw Ethanol 19738.00
+    // 14:37:57.103 -> Light: 80.00 lx
+    // 14:37:57.103 -> Bat: 4.26 V
+}
+
+void at_init()
+{
+    DBG_OUT.println(F("void at_init()"));
+
+    sendData("AT", 1000, DEBUG);
+    delay(1000);
+    sendData("AT+SIMCOMATI", 1000, DEBUG);
+    delay(1000);
+    sendData("AT+CICCID", 1000, DEBUG);
+    sendData("AT+CNUM", 1000, DEBUG);
+    sendData("AT+COPS?", 1000, DEBUG);
+    sendData("AT+CPSI?", 1000, DEBUG);
+    sendData("AT+CSQ", 1000, DEBUG);
+
+    
+}
+
+void http_request()
+{
+    // Http test
+    sendData("AT+HTTPINIT", 1000, DEBUG);
+     sendData("AT+HTTPPARA=\"URL\",\"http://www.baidu.com\"", 1000, DEBUG);
+    //sendData("AT+HTTPPARA=\"URL\",\"http://api.thingspeak.com/update?api_key=5WOQ0JZURMWBTRF4&field1=123&field2=456\"", 1000, DEBUG);
+    sendData("AT+HTTPACTION=0", 1000, DEBUG);
+    sendData("AT+CSQ", 1000, DEBUG);
+    sendData("AT+HTTPHEAD", 1000, DEBUG);
+    sendData("AT+HTTPREAD=0,500", 1000, DEBUG);
+    sendData("AT+HTTPTERM", 1000, DEBUG);
+}
+
+String sendData(String command, const int timeout, boolean debug)
+{
+    String response = "";
+    mySerial2.println(command);
+    long int time = millis();
+    while ((time + timeout) > millis())
+    {
+        while (mySerial2.available())
+        {
+            char c = mySerial2.read();
+            response += c;
+        }
+    }
+    if (debug)
+    {
+        DBG_OUT.print(response);
+    }
+    return response;
+}
